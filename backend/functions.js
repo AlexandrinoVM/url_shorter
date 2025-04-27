5a6a38bd95885a52ec8c1d0c88b4f43e87f750e2
AlexandrinoVM <vinicius.moraes2@outlook.com>
2025-04-27 14:55:16 -0300

    first commit

---
+++ backend/functions.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const fs = require('fs')
+const crypto = require('crypto')
+
+function getDbPath(){
+    return path.join(__dirname,'database.json')
+}
+
+function insertData(data){
+    fs.writeFileSync(getDbPath(), JSON.stringify(data, null, 2))
+}
+
+function GenerateShortUrl(data) {
+    let urlDomain =""
+    let count =0
+    console.log(data)
+    for(let i =0;i<data.length;i++){
+        if(data[i] === '.'){
+            count ++
+            if(count == 1){
+                continue
+            } 
+            if(count === 2){
+                break
+            }
+        }
+        if(count === 1){
+            urlDomain += data[i]
+        }
+    }
+     let domain = urlDomain + "/"+crypto.randomBytes(3).toString('hex');
+    console.log(domain)
+    return domain;
+}
+
+function getShortUrl(data){
+
+}
+
+function InsertOnDb(data){
+    
+}
+
+function ReadDb(){
+    const path = getDbPath()
+    if(!path) return {}
+    const data = fs.readFileSync(path,'utf-8')
+    try{
+        if (!data) {
+            fs.writeFileSync(path, JSON.stringify({}), 'utf8');
+            return {};
+          }
+          return JSON.parse(data)
+    }catch(err){
+        console.error('Erro ao ler ou parsear o arquivo JSON:', err);
+        return {}; 
+      
+    }
+}
+
+module.exports = {
+    insertData,
+    ReadDb,
+    GenerateShortUrl
+}
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+const functions = require('./functions.js')
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/:domain/:id',(req,res)=>{
+  const  {domain,id}  = req.params;
+  const database = functions.ReadDb();
+  const originalUrl = database[domain+"/"+id];
+  console.log(originalUrl)
+  if(originalUrl){
+    res.redirect(originalUrl)
+  }else{
+    res.status(404).send('URL not Found')
+  }
+})
+
+app.post('/shortUrl/shorten',(req,res)=>{
+  const {originalUrl} = req.body
+  const database = functions.ReadDb();
+  const shortUrl = functions.GenerateShortUrl(JSON.stringify(originalUrl))
+  database[shortUrl] = originalUrl
+  functions.insertData(database)
+
+  res.json({shortUrl: `http://localhost:3000/${shortUrl}`})
+})
+
+app.listen(port,()=>{
+  console.log(`listen ${port}`)
+})
